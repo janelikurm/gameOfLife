920d51cba32a29aab36157c174979f6850bdf8e5
Janeli <janelikurm@gmail.com>
2023-05-16 12:15:52 +0300

    extracted select option box to separate component and added one more pattern

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,8 @@
 <script>
 
     import {onMount} from 'svelte';
+    import Patterns from "./Patterns.svelte";
 
-    let patterns = [
-        {id: 1, text: `--Patterns--`},
-        {id: 2, text: `Cloverleaf`},
-        {id: 3, text: `something`},
-
-    ];
-
-    let selected;
     let rows = 25
     let cols = 25
     let grid = []
@@ -19,7 +12,6 @@
     let intervalId
     let gameOn = false
     let speed
-
 
     onMount(async () => {
         await createGameField()
@@ -168,75 +160,6 @@
         }
     }
 
-    async function cloverleaf() {
-        await clearGameField()
-        grid[8][11] = 1
-        grid[8][13] = 1
-        grid[9][9] = 1
-        grid[9][10] = 1
-        grid[9][11] = 1
-        grid[9][13] = 1
-        grid[9][14] = 1
-        grid[9][15] = 1
-        grid[10][8] = 1
-        grid[10][12] = 1
-        grid[10][16] = 1
-        grid[11][8] = 1
-        grid[11][10] = 1
-        grid[11][14] = 1
-        grid[11][16] = 1
-        grid[12][9] = 1
-        grid[12][10] = 1
-        grid[12][12] = 1
-        grid[12][14] = 1
-        grid[12][15] = 1
-        grid[14][9] = 1
-        grid[14][10] = 1
-        grid[14][12] = 1
-        grid[14][14] = 1
-        grid[14][15] = 1
-        grid[15][8] = 1
-        grid[15][10] = 1
-        grid[15][14] = 1
-        grid[15][16] = 1
-        grid[16][8] = 1
-        grid[16][12] = 1
-        grid[16][16] = 1
-        grid[17][9] = 1
-        grid[17][10] = 1
-        grid[17][11] = 1
-        grid[17][13] = 1
-        grid[17][14] = 1
-        grid[17][15] = 1
-        grid[18][11] = 1
-        grid[18][13] = 1
-        await updateView()
-    }
-
-    async function beacon() {
-        await clearGameField()
-        grid[10][11] = 1;
-        grid[10][12] = 1;
-        grid[11][11] = 1;
-        grid[12][14] = 1;
-        grid[13][13] = 1;
-        grid[13][14] = 1;
-        await updateView()
-    }
-
-    function handleSelect() {
-        switch (selected) {
-            case '--Patterns--':
-                clearGameField()
-                break
-            case 'Cloverleaf':
-                cloverleaf()
-                break
-            case 'Beacon':
-                beacon()
-                break
-        }
-    }
 
 </script>
 <main>
@@ -244,6 +167,7 @@
         <h1>
             Game of Life
         </h1>
+
         <div class="button-row1">
             <button class="button" on:click={() => gameOn = true} on:click={automaticNextGeneration}>Start</button>
             <button class="button" on:click={() => gameOn = false} on:click={stopGame}>Stop</button>
@@ -259,35 +183,15 @@
         <table id="gameField"></table>
 
         <label for="customRange" class="form-label">Speed</label>
-        <input on:change={changeSpeed} type="range" id="customRange" min="100" max="1000" value="550">
+        <input on:change={changeSpeed} type="range" id="customRange" min="20" max="700" value="360">
 
         <div>
-            <select class="custom-select" bind:value={selected} on:change={handleSelect}>
-                <option selected>--Patterns--</option>
-                <option>Cloverleaf</option>
-                <option>Beacon</option>
-            </select>
-
+            <Patterns {updateView} {grid} {clearGameField}/>
         </div>
-
-
     </div>
-    <div class="footer"></div>
 
 </main>
 <style>
-
-
-    .custom-select {
-        position: relative;
-        margin: 1em;
-        border-radius: 3px;
-        width: 130px;
-        height: 25px;
-        text-align: center;
-
-    }
-
     .button-row1 {
         display: flex;
         justify-content: center;
@@ -298,7 +202,6 @@
         display: flex;
         justify-content: center;
         margin: 0 2em 0.8em;
-
     }
 
     label {
@@ -332,12 +235,5 @@
         font-family: AppleMyungjo, serif;
     }
 
-    .button:hover {
-        filter: drop-shadow(0 0 2em #eaa9a9);
-    }
-
-    .footer {
-        padding-bottom: 50px;
-    }
 
 </style>
---
+++ src/Patterns.svelte
@@ -0,0 +1,121 @@
+<script>
+
+export let clearGameField
+export let updateView
+export let grid
+let selected
+
+function handleSelect() {
+    switch (selected) {
+        case '--Patterns--':
+            clearGameField()
+            break
+        case 'Cloverleaf':
+            cloverleaf()
+            break
+        case 'Beacon':
+            beacon()
+            break
+        case 'Spaceship':
+            spaceship()
+            break
+    }
+}
+
+async function cloverleaf() {
+    await clearGameField()
+    grid[8][11] = 1
+    grid[8][13] = 1
+    grid[9][9] = 1
+    grid[9][10] = 1
+    grid[9][11] = 1
+    grid[9][13] = 1
+    grid[9][14] = 1
+    grid[9][15] = 1
+    grid[10][8] = 1
+    grid[10][12] = 1
+    grid[10][16] = 1
+    grid[11][8] = 1
+    grid[11][10] = 1
+    grid[11][14] = 1
+    grid[11][16] = 1
+    grid[12][9] = 1
+    grid[12][10] = 1
+    grid[12][12] = 1
+    grid[12][14] = 1
+    grid[12][15] = 1
+    grid[14][9] = 1
+    grid[14][10] = 1
+    grid[14][12] = 1
+    grid[14][14] = 1
+    grid[14][15] = 1
+    grid[15][8] = 1
+    grid[15][10] = 1
+    grid[15][14] = 1
+    grid[15][16] = 1
+    grid[16][8] = 1
+    grid[16][12] = 1
+    grid[16][16] = 1
+    grid[17][9] = 1
+    grid[17][10] = 1
+    grid[17][11] = 1
+    grid[17][13] = 1
+    grid[17][14] = 1
+    grid[17][15] = 1
+    grid[18][11] = 1
+    grid[18][13] = 1
+    await updateView()
+}
+
+async function beacon() {
+    await clearGameField()
+    grid[10][11] = 1
+    grid[10][12] = 1
+    grid[11][11] = 1
+    grid[12][14] = 1
+    grid[13][13] = 1
+    grid[13][14] = 1
+    await updateView()
+}
+
+async function spaceship() {
+    await clearGameField()
+    grid[8][10] = 1
+    grid[8][11] = 1
+    grid[8][14] = 1
+    grid[8][15] = 1
+    grid[9][12] = 1
+    grid[9][13] = 1
+    grid[10][12] = 1
+    grid[10][13] = 1
+    grid[11][9] = 1
+    grid[11][11] = 1
+    grid[11][14] = 1
+    grid[11][16] = 1
+    grid[12][9] = 1
+    grid[12][16] = 1
+    grid[14][9] = 1
+    grid[14][16] = 1
+    grid[15][10] = 1
+    grid[15][11] = 1
+    grid[15][14] = 1
+    grid[15][15] = 1
+    grid[16][11] = 1
+    grid[16][12] = 1
+    grid[16][13] = 1
+    grid[16][14] = 1
+    grid[18][12] = 1
+    grid[18][13] = 1
+    grid[19][12] = 1
+    grid[19][13] = 1
+    await updateView()
+}
+
+</script>
+
+<select class="custom-select" bind:value={selected} on:change={handleSelect}>
+    <option selected>--Patterns--</option>
+    <option>Cloverleaf</option>
+    <option>Beacon</option>
+    <option>Spaceship</option>
+</select>
